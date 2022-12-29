b0dae4fbd15cadc6e043b2ebdf1d17de275fef6f
vtekhtilov <vyacheslav.tekhtilov@dataart.com>
2022-12-29 15:33:46 +0100

    added js executor to remove ads from page

--- src/main/java/steps/BaseSteps.java
+++ src/main/java/steps/BaseSteps.java
@@ -1,14 +1,12 @@
 package steps;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.SelenideElement;
-import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.testng.Assert;
 import utils.Config;
-import utils.FakerControl;
-import utils.FilterControl;
+
+
 
 import java.time.Duration;
 
@@ -16,34 +14,54 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class BaseSteps {
 
-    public void openBaseUrl()  {
+    public void openBaseUrl() {
         Selenide.open(Config.getBaseURL());
         WebDriverRunner.getWebDriver().manage().window().maximize();
     }
-    public void clickOnElement(SelenideElement element){
+
+    public void clickOnElement(SelenideElement element) {
         $(element).click();
     }
 
-    public void pageElementAccert(SelenideElement element, String expectedText){
-        Assert.assertEquals(element.getText(),expectedText, "Actual text does not match expected one");
+    public void pageElementAccert(SelenideElement element, String expectedText) {
+        Assert.assertEquals(element.getText(), expectedText, "Actual text does not match expected one");
     }
 
-    public void waitTillElementIsVisible(SelenideElement element){$(element).shouldBe(Condition.visible, Duration.ofSeconds(6));
+    public void waitTillElementIsVisible(SelenideElement element) {
+        $(element).shouldBe(Condition.visible, Duration.ofSeconds(6));
     }
-    public void waitTillElementDisappears(SelenideElement element){$(element).shouldNotBe(Condition.visible);}
-    public void waitTillElementIsEnabled(SelenideElement element){$(element).should(Condition.enabled, Duration.ofSeconds(6));}
-    public void scrollTillElementIsVisible(SelenideElement element){$(element).scrollIntoView(true);}
+
+    public void waitTillElementDisappears(SelenideElement element) {
+        $(element).shouldNotBe(Condition.visible);
+    }
+
+    public void waitTillElementIsEnabled(SelenideElement element) {
+        $(element).should(Condition.enabled, Duration.ofSeconds(6));
+    }
+
+    public void scrollTillElementIsVisible(SelenideElement element) {
+        $(element).scrollIntoView(true);
+    }
 
 
-    public void pageElementAccertContains(SelenideElement element, String expectedText){
+    public void pageElementAccertContains(SelenideElement element, String expectedText) {
         Assert.assertTrue(element.getText().contains(expectedText), "result does not contain expected Text");
     }
-    public void checkCurrentURL(String expectedURL){
+
+    public void checkCurrentURL(String expectedURL) {
         Assert.assertEquals(WebDriverRunner.getWebDriver().getCurrentUrl(), expectedURL);
     }
-//    public void closeIframeAdvertisement(String frameId){
-//        switchTo().frame(frameId, Duration.ofSeconds(10)).close();
-//        switchTo().defaultContent();
-//    }
 
+    public void closeIframeAdvertisement(String frameId) {
+        switchTo().frame(frameId, Duration.ofSeconds(10)).close();
+        switchTo().defaultContent();
+    }
+    public void setAdsHidden(){
+       // SelenideElement ads = $(By.xpath("//iframe[@title='3rd party ad content']"));
+
+        //JavascriptExecutor js = (JavascriptExecutor) Selenide.webdriver();
+        //SelenideElement ads = $(By.xpath("//div[@id ='fixedban']"));
+        //js.executeScript("return document.getElementById('fixedban').remove();");
+        Selenide.executeJavaScript("return document.getElementById('fixedban').remove();");
+    }
 }
--- src/test/java/ElementsTextBoxTest.java
+++ src/test/java/ElementsTextBoxTest.java
@@ -1,18 +1,20 @@
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
 import jdk.jfr.Description;
+import lombok.SneakyThrows;
 import org.openqa.selenium.By;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.switchTo;
 
 public class ElementsTextBoxTest extends BaseTest {
+    @SneakyThrows
     @BeforeMethod
     public void openElementsPage(){
         baseSteps.openBaseUrl();
-        baseSteps.scrollTillElementIsVisible(mainPage.elementsButton());
-        baseSteps.waitTillElementIsVisible(mainPage.elementsButton());
-        //Selenide.switchTo().frame($(By.xpath("//iframe[@id='google_ads_iframe_/21849154601,22343295815/Ad.Plus-Anchor_0']")))
+        baseSteps.setAdsHidden();
         mainPageSteps.clickOnElement(mainPage.elementsButton());
         mainPageSteps.clickOnElement(elementsPage.textBoxSection());
         mainPageSteps.elementsTEXTBOXPageTitleAssertion();
